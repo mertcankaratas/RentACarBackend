9a7c8f35f568822542f6ed7ec6a6f6f1048dd789
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-10-05 14:40:39 +0300

    JWT Adding

--- src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -23,6 +23,13 @@ namespace Persistence
             services.AddScoped<IBrandRepository, BrandRepository>();
             services.AddScoped<IModelRepository, ModelRepository>();
 
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+            services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
+
+
             return services;
         }
     }
